a9d9ebc4747bb5e9c5179277867eade07e7b7d22
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-09-26 11:28:06 +0200

    add value for input in ContactForm

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -34,6 +34,7 @@ class ContactForm extends Component<Props, State> {
   };
 
   render() {
+    const { name, number } = this.state;
     return (
       <form onSubmit={this.submitForm}>
         <div className="formInputWrapper">
@@ -43,6 +44,7 @@ class ContactForm extends Component<Props, State> {
             onChange={this.handleChange}
             type="text"
             name="name"
+            value={name}
             pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
@@ -55,6 +57,7 @@ class ContactForm extends Component<Props, State> {
             onChange={this.handleChange}
             type="tel"
             name="number"
+            value={number}
             pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
