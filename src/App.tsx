d0752d748ad064a512d206872e195286e3ef9e2d
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-09-26 11:00:55 +0200

    add component ContactForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import './App.css';
+import ContactForm from './components/ContactForm/ContactForm';
 import { nanoid } from 'nanoid';
 
 interface IContact {
@@ -11,23 +12,15 @@ interface IContact {
 interface State {
   contacts: IContact[];
   filter: string;
-  name: string;
-  number: string;
 }
 
 class App extends Component<{}, State> {
   state = {
     contacts: [],
     filter: '',
-    name: '',
-    number: '',
   };
 
-  submitForm = (evt: React.FormEvent<HTMLFormElement>) => {
-    evt.preventDefault();
-
-    const { name, number } = this.state;
-
+  creationContact = (name: string, number: string) => {
     const contact: IContact = {
       name: name,
       number: number,
@@ -37,19 +30,6 @@ class App extends Component<{}, State> {
     this.setState(prev => ({
       contacts: [...prev.contacts, contact],
     }));
-  };
-
-  handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = evt.currentTarget;
-
-    switch (name) {
-      case 'name':
-        this.setState({ name: value });
-        break;
-      case 'number':
-        this.setState({ number: value });
-        break;
-    }
   };
 
   handleFilter = (evt: React.ChangeEvent<HTMLInputElement>) => {
@@ -71,35 +51,9 @@ class App extends Component<{}, State> {
 
     return (
       <div className="App">
-        <h2>Phonebook</h2>
-        <form onSubmit={this.submitForm}>
-          <div className="formInputWrapper">
-            <label htmlFor="name">Name </label>
-            <input
-              id="name"
-              onChange={this.handleChange}
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </div>
-          <div className="formInputWrapper">
-            <label htmlFor="number">Number</label>
-            <input
-              id="number"
-              onChange={this.handleChange}
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-            />
-          </div>
+        <h1>Phonebook</h1>
+        <ContactForm creationContact={this.creationContact} />
 
-          <button type="submit">Add Contact</button>
-        </form>
         <h2>Contacts</h2>
         <p>Find contacts by name</p>
         <input type="text" onChange={this.handleFilter} />
